398df591085e2ed2382d9404463af72f12073342
Dmitriy Melnik <ml.dmitriymelnik@yandex.ru>
2022-05-06 18:24:21 +0300

    ConfigModule

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from './ormconfig';
+
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(ormconfig),
+    CatsModule,
+  ],
 })
 export class AppModule {}
